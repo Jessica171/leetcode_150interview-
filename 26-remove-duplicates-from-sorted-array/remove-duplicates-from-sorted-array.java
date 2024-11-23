7a20b31986ae3d7cc458905e5e30c43d3ca623a6
Jessica Mamdouh <82265812+Jessica171@users.noreply.github.com>
2024-11-23 19:49:13 +0200

    Time: 7 ms (5.12%) | Memory: 45.3 MB (7.47%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -1,24 +1,12 @@
+import java.util.*;
+
 class Solution {
     public int removeDuplicates(int[] nums) {
-        if (nums.length == 0) {
-            return 0;
+        Set<Integer> set = new LinkedHashSet<>(Arrays.stream(nums).boxed().toList());
+        int k = 0;
+        for (int num : set) {
+            nums[k++] = num;
         }
-
-        // Pointer to write the next unique element
-        int write = 1;
-
-        // Traverse the array starting from the second element
-        for (int read = 1; read < nums.length; read++) {
-            // If the current element is different from the previous one, it is unique
-            if (nums[read] != nums[read - 1]) {
-                // Write the unique element at the write pointer position
-                nums[write] = nums[read];
-                // Move the write pointer to the next position
-                write++;
-            }
-        }
-
-        // The write pointer indicates the number of unique elements
-        return write;
+    return k;
     }
 }
